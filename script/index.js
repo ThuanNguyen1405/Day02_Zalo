83bb7b21e2be2a0b477deb2dd751610fdf0dcf99
ThuanNguyen1405 <thuan.nt00900@sinhvien.hoasen.edu.vn>
2023-02-01 15:46:03 +0700

    D02-alo

---
+++ script/index.js
@@ -0,0 +1,38 @@
+/*secretNum = 1 + Math.ceil(Math.random() * 99);
+console.log("So bi mat la: "+secretNum);
+while(true){
+    let input = prompt("Doan so bi mat: ");
+    let guess = parseInt(input);
+    if(guess == secretNum)
+    {
+        alert("Ban da doan dung");
+        break;
+    }
+    if(guess < secretNum)
+    {
+        alert("So bi an lon hon so cua ban");
+    }
+    else{
+        alert("So bi an nho hon so cua ban");
+    }
+}*/
+/*
+const choice= ["s", "p", "r"];
+let randomN = Math.ceil(Math.random()*1);
+let randomCpt = choice[randomN];
+console.log("random: "+randomN);
+console.log("Lua chon cua may tinh: "+randomCpt);
+    let input = prompt("Nhap lua chon cua ban:");
+    let Uchoice = choice[parseInt(input)];
+    console.log(Uchoice);
+    if(Uchoice + randomCpt == rs || Uchoice + randomCpt == pr ||Uchoice + randomCpt == sp)
+    {
+        alert("Win");
+    }
+    else if(Uchoice + randomCpt == rp || Uchoice + randomCpt == ps ||Uchoice + randomCpt == sr)
+    {
+        alert("Lose");
+    }
+    else{
+        alert("draw");
+    }*/
